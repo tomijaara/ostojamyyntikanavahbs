d8fb2c774318e7197e5f731be7d1e0a968fe4ec8
Tomi <tomi.jaara@gmail.com>
2022-03-27 22:17:56 +0300

    Trying middleware as it own file

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ import register from './routes/register.routes.js';
 import auth from './routes/auth.routes.js';
 import hbsHelpers from './helpers/handlebars-helpers.js';
 import fileUpload from 'express-fileupload';
+import multer from 'multer'
+import path from 'path';
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'upload');
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+
+});
+
+const upload = multer({ storage: storage });
 
 dotenv.config();
 
@@ -42,7 +57,7 @@ app.use(cookieParser());
 
 app.use(fileUpload());
 
-app.use('/', pages);
+app.use('/', upload.single('sampleFile'), pages);
 app.use('/register', register);
 app.use('/auth', auth);
 
--- routes/page.routes.js
+++ routes/page.routes.js
@@ -9,6 +9,8 @@ import newAdvert, {
 } from '../controllers/advert.controller.js';
 import { listUsers } from '../controllers/user.controller.js';
 
+
+
 const routes = express.Router();
 
 routes.get('/', [isLoggedIn, listAdverts, listUsers], (req, res) => {
@@ -45,72 +47,50 @@ routes.get('/newAdvert', isLoggedIn, (req, res) => {
 });
 
 routes.post('/newAdvert', [isLoggedIn, newAdvert], (req, res) => {
-    let sampleFile;
-    let uploadPath;
-    console.log("hello" + req.files);
-    if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were uploaded.');
-    }
+    res.render('new-advert', {
+        user: req.user
+    });
+});
 
-    // name of the input is sampleFile
-    sampleFile = req.files.sampleFile;
-    uploadPath = __dirname + '/upload/' + sampleFile.name;
-
-    console.log(sampleFile);
-
-    // Use mv() to place file on the server
-    sampleFile.mv(uploadPath, function (err) {
-        if (err) return res.status(500).send(err);
-    }),
-
-
-
-
-
-        res.render('new-advert', {
+routes.get('/profile', [isLoggedIn, listUserAdverts], (req, res) => {
+    if (req.list) {
+        res.render('profile', {
+            list: req.list,
             user: req.user
         });
-    });
-
-    routes.get('/profile', [isLoggedIn, listUserAdverts], (req, res) => {
-        if (req.list) {
-            res.render('profile', {
-                list: req.list,
-                user: req.user
-            });
-        } else {
-            res.redirect('/login');
-        }
+    } else {
+        res.redirect('/login');
     }
-    );
+}
+);
 
-    routes.get('/editAdvert/:id', [isLoggedIn, getAdvert], (req, res) => {
-        const user = req.user;
-        const advert = req.advert
-        if (advert) {
-            res.render('edit-advert', {
-                user,
-                advert
-            });
-        } else {
-            res.redirect('/login');
-        }
-    });
+routes.get('/editAdvert/:id', [isLoggedIn, getAdvert], (req, res) => {
+    const user = req.user;
+    const advert = req.advert
+    if (advert) {
+        res.render('edit-advert', {
+            user,
+            advert
+        });
+    } else {
+        res.redirect('/login');
+    }
+});
 
-    routes.post('/editAdvert/:id', [isLoggedIn, updateAdvert], (req, res) => {
-        const user = req.user;
-        const advert = req.advert
-        if (advert) {
-            res.render('edit-advert', {
-                user,
-                advert
-            });
-        } else {
-            res.redirect('/login');
-        }
-    });
+routes.post('/editAdvert/:id', [isLoggedIn, updateAdvert], (req, res) => {
+    const user = req.user;
+    const advert = req.advert
+    if (advert) {
+        res.render('edit-advert', {
+            user,
+            advert
+        });
+    } else {
+        res.redirect('/login');
+    }
+});
 
-    routes.get('/deleteAdvert/:id', deleteAdvert);
+routes.get('/deleteAdvert/:id', deleteAdvert);
 
 
-    export default routes;
+export default routes;
